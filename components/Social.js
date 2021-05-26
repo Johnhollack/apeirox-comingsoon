ea767b94771ff60fc24fbd2813a505436bf320fe
johnhollack <adejumolekan@gmail.com>
2021-05-26 01:59:37 +0100

    updated social handles

--- components/Social.js
+++ components/Social.js
@@ -4,9 +4,9 @@ import SocialItem from "./SocialItem";
 function Social() {
     return (
         <div className="flex flex-grow mt-12 justify-evenly max-w-1xl items-center">
-            <SocialItem title="@apeiroxHQ" image="/facebook.png" url="https://facebook.com/apeiroxcom"/>
-            <SocialItem title="@apeiroxHQ" image="/twitter.png" url="https://twitter.com/apeiroxcom"/>
-            <SocialItem title="@apeiroxHQ" image="/instagram.png" url="https://instagram.com/apeiroxcom"/>
+            <SocialItem title="@apeiroxcom" image="/facebook.png" url="https://facebook.com/apeiroxcom"/>
+            <SocialItem title="@apeiroxcom" image="/twitter.png" url="https://twitter.com/apeiroxcom"/>
+            <SocialItem title="@apeiroxcom" image="/instagram.png" url="https://instagram.com/apeiroxcom"/>
         </div>
     )
 }
